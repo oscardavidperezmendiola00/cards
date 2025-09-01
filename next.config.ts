a469e7ca28ff7c37edf575c1f5fa4bfd636a9e73
oscardavidperezmendiola00 <oscar.pmendiola@gmail.com>
2025-09-01 17:21:47 -0600

    feat: allow Supabase images + fix slug page

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-};
+const nextConfig: NextConfig = { images: { remotePatterns: [ { protocol: 'https', hostname: 'https://bvdjkgpxaypwmilxrdky.supabase.co', pathname: '/storage/v1/object/public/**', }, ], }, };
 
 export default nextConfig;
+
